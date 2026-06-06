d9861dc4552cc000d5bac4053284aea2623e6c16
sahil-sanjay-kadam <kadam.sahil.sanjay@gmail.com>
2026-06-06 13:13:00 +0530

    Login and add to cart test

--- tests/tests-1.spec.ts
+++ tests/tests-1.spec.ts
@@ -1,9 +1,20 @@
 import { test } from '@playwright/test'
 
-test('login to saucedemo', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('')
     // login
     await page.getByRole('textbox', {name: 'Username'}).fill('standard_user')
     await page.getByRole('textbox', {name: 'Password'}).fill('secret_sauce')
     await page.getByRole('button', {name: 'Login'}).click()
+
+})
+
+test('saucedemo test 1', async ({ page }) => {
+    // add to cart
+    await page.locator('.inventory_item')
+    .filter({ hasText: "Sauce Labs Backpack"})
+    .getByRole('button', { name: 'Add to cart'})
+    .click()
+
+    
 })
